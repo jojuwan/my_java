1943bdaf555138b053569e6b1b1543fc833a009e
jojuwan <whwndhks123@naver.com>
2022-10-30 19:37:49 +0900

    home Git-Hub

---
+++ Step07_Array/src/test/main/QuizMain.java
@@ -0,0 +1,28 @@
+package test.main;
+
+import java.util.Random;
+
+public class QuizMain {
+	public static void main(String[] args) {
+		/*
+		 * 1. run 했을때
+		 * 	  cherry, apple, banana, melon, 7
+		 * 	  5개의 문자열 중에서 1개가 랜덤하게 출력되게 해 보세요.
+		 */
+		String[] items= {"cherry", "apple", "banana", "melon", "7"};
+		//랜덤한 숫자를 얻어내기 위한 객체
+		Random ran=new Random();
+		// 0~4 사이의 랜덤한 숫자를 하나 얻어내서
+		int ranNum=ran.nextInt(5);
+		// 배열의 인덱스로 활용해서 문자열 출력하기
+		System.out.println(items[ranNum]);
+		
+		int ranNum2=ran.nextInt(5);
+		int ranNum3=ran.nextInt(5);
+		System.out.println(items[ranNum]+" | "+ items[ranNum2]+" | "+ items[ranNum3]);
+		
+		if(ranNum == ranNum2 && ranNum2 == ranNum3) {
+			System.out.println("777");
+		};
+	}
+}
---
+++ Step09_AbstractClass/src/test/mypack/Gun.java
@@ -0,0 +1,11 @@
+package test.mypack;
+
+public class Gun extends Weapon{
+
+	@Override
+	public void attack() {
+		System.out.println("총으로 공격해요! 빵야~");
+		
+	}
+
+}
