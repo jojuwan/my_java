9c58c297e6c99363f07abef7fe91e0664cacbe18
jojuwan <whwndhks123@naver.com>
2022-10-31 11:49:23 +0900

    Step10 예제 추가함

--- Step09_AbstractClass/src/test/main/MainClass05.java
+++ Step09_AbstractClass/src/test/main/MainClass05.java
@@ -29,7 +29,7 @@ public class MainClass05 {
 	
 	
 	public static void main(String[] args) {
-		
+		// 메소드 호출하면서 static 필드에 미리 준비된 값을 전달하기
 		useWeapon(w1);
 		// Anonymous Local Inner Class 를 이용해서 Weapon type 의 참조값 얻어내기 
 		Weapon w2=new Weapon() {
@@ -39,9 +39,9 @@ public class MainClass05 {
 				System.out.println("공격");
 			} 
 		};
-		 
+		 //메소드 호출하면서 지역변수에 미리 준비된 값을 전달하기
 		 useWeapon(w2);
-		 
+		 //메소드 호출하면서 값을 즉석에서 만들어서 전달하기
 		 useWeapon(new Weapon() {
 
 			@Override
